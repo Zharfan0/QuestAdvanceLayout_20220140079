db012bf3473ae3984e4cbf4b0f282f96672936a7
Zharfan <zharfanwidyadana@gmail.com>
2025-10-23 15:22:56 +0700

    memindah row dan column

--- app/src/main/java/com/example/pam4/UIPertama.kt
+++ app/src/main/java/com/example/pam4/UIPertama.kt
@@ -31,44 +31,43 @@ fun ActivityPertama(modifier:Modifier){
         .padding(top=100.dp)
         .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = stringResource(id = R.string.prodi),
-            fontSize = 35.sp,
-            fontWeight = FontWeight.Bold)
-        Text(text = stringResource(id = R.string.univ),
-            fontSize = 22.sp)
-        Spacer(modifier= Modifier.height(25.dp))
-        Card (modifier = Modifier
-            .fillMaxWidth(1f)
-            .padding(12.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = Color.DarkGray
-            )){}
-    }
-
-    Row () {
-        val gambar = painterResource(R.drawable.logo_ugm)
-        Image(
-            painter = gambar,
-            contentDescription = null,
-            modifier = Modifier
-                .size(100.dp)
-                .padding(5.dp)
-        )
-        Spacer(modifier = Modifier.width(30.dp))
-        Column {
-            Text(
-                text = stringResource(R.string.nama),
-                fontSize = 30.sp,
-                fontFamily = FontFamily.Cursive,
-                color = Color.White,
-                modifier = Modifier.padding(top=15.dp)
+        ) {
+            Text(text = stringResource(id = R.string.prodi),
+                fontSize = 35.sp,
+                fontWeight = FontWeight.Bold)
+            Text(text = stringResource(id = R.string.univ),
+                fontSize = 22.sp)
+            Spacer(modifier= Modifier.height(25.dp))
+            Card (modifier = Modifier
+                .fillMaxWidth(1f)
+                .padding(12.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.DarkGray
+                )){}
+        Row () {
+            val gambar = painterResource(R.drawable.logo_ugm)
+            Image(
+                painter = gambar,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(5.dp)
             )
+            Spacer(modifier = Modifier.width(30.dp))
+            Column {
+                Text(
+                    text = stringResource(R.string.nama),
+                    fontSize = 30.sp,
+                    fontFamily = FontFamily.Cursive,
+                    color = Color.White,
+                    modifier = Modifier.padding(top=15.dp)
+                )
+            }
         }
-    }
-    Row {
-        Column {
+        Row {
+            Column {
 
+            }
         }
     }
 }
