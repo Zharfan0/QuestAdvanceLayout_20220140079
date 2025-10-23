67aa793d06ed2a1008aea9de672110e7f20fb74e
Zharfan <zharfanwidyadana@gmail.com>
2025-10-24 00:30:09 +0700

    membesarkan logo UI

--- app/src/main/java/com/example/pam4/UIPertama.kt
+++ app/src/main/java/com/example/pam4/UIPertama.kt
@@ -136,7 +136,7 @@ fun ActivityPertama(modifier:Modifier){
                     painter = gambar,
                     contentDescription = null,
                     modifier = Modifier
-                        .size(100.dp)
+                        .size(200.dp)
                         .padding(5.dp)
                 )
                 Spacer(modifier = Modifier.width(30.dp))
