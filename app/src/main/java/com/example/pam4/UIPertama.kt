1ad27235c3ef6c7521281d18861ed933e0a51eac
Zharfan <zharfanwidyadana@gmail.com>
2025-10-23 15:26:00 +0700

    memberi properties string alamat

--- app/src/main/java/com/example/pam4/UIPertama.kt
+++ app/src/main/java/com/example/pam4/UIPertama.kt
@@ -69,7 +69,10 @@ fun ActivityPertama(modifier:Modifier){
             Column () {
             }
             Text(
-                text = stringResource(R.string.alamat)
+                text = stringResource(R.string.alamat),
+                fontSize = 20.sp,
+                color = Color.Yellow,
+                modifier = Modifier.padding(top=10.dp)
             )
         }
     }
