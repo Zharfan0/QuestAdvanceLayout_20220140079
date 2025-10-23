f944ea9b0ee65596c5166dcb7b254b27d754e5b6
Zharfan <zharfanwidyadana@gmail.com>
2025-10-23 23:54:07 +0700

    membuat Card 3

--- app/src/main/java/com/example/pam4/UIPertama.kt
+++ app/src/main/java/com/example/pam4/UIPertama.kt
@@ -116,6 +116,44 @@ fun ActivityPertama(modifier:Modifier){
 
             }
         }
+        Card (modifier = Modifier
+            .fillMaxWidth(1f)
+            .padding(12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )){
+            Row () {
+                val gambar = painterResource(R.drawable.logo_ugm)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .padding(5.dp)
+                )
+                Spacer(modifier = Modifier.width(30.dp))
+                Column {
+                    Text(
+                        text = stringResource(R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top=15.dp)
+                    )
+                }
+            }
+            Row () {
+                Column () {
+                }
+                Text(
+                    text = stringResource(R.string.alamat),
+                    fontSize = 20.sp,
+                    color = Color.Yellow,
+                    modifier = Modifier.padding(top=10.dp)
+                )
+
+            }
+        }
     }
     Box(
         modifier = Modifier
